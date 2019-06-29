9162f28eb317a0ecec3d179ec185a3e564109c5c
yFernandess <fernandes.yuri@hotmail.com>
2019-06-29 12:01:38 -0300

    First commit

---
+++ src/sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,16 @@
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+	
+		double salario = 1250.70;
+		System.out.println("Meu salário é: " + "R$" + salario);
+		
+		//Por mais que a variavel seja do tipo double o compilador ira cuspir o resultado 2.0
+		double divisao = 5 / 2;
+		System.out.println(divisao);
+		
+		//Um dos valores da operação deve estar no formato double: 5.0
+		double divisao2 = 5.0 / 2;
+		System.out.println(divisao2);
+	}
+}
