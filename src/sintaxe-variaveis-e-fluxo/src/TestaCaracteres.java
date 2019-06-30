f4e73eb5b7553f87cb22e03078bfec9918c80a49
yFernandess <fernandes.yuri@hotmail.com>
2019-06-30 18:21:48 -0300

    Testa Caracteres

---
+++ src/sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,24 @@
+
+public class TestaCaracteres {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String sValue = "Teste";
+		System.out.println(sValue);
+		
+		sValue = sValue + 2020;
+		System.out.println(sValue);
+		
+		String parc01 = "10";
+		String parc02 = "20";
+		System.out.println(parc01+parc02);
+	}
+}
---
+++ src/sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,18 @@
+
+public class TestaValores {
+
+	public static void main(String[] args) {
+		int primeiro = 5;
+		int segundo = 7;
+		segundo = primeiro;
+		primeiro = 10;
+		System.out.println(segundo);
+		
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Yuri ";
+		String continuacao = "e minha idade é ";
+		int idade = 100;
+		System.out.println(saudacao+nome+continuacao+idade);
+	}
+
+}
