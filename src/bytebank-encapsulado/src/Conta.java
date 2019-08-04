f95bd1d471392afa315fd75ca88faddccf85bdad
yFernandess <fernandes.yuri@hotmail.com>
2019-08-04 17:42:22 -0300

    Constructor

--- src/bytebank-encapsulado/src/Conta.java
+++ src/bytebank-encapsulado/src/Conta.java
@@ -3,6 +3,15 @@ public class Conta {
 	private int agencia;
 	private int numero;
 	private Cliente titular;
+	private static int total;
+	
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		//System.out.println("O total de contas é: " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		//System.out.println("Estou criando uma conta " + this.numero);
+	}
 	
 	public void depositar(double valor) {
 		this.saldo += valor;
@@ -29,28 +38,20 @@ public class Conta {
 		return this.saldo;
 	}
 	
-	public int getNumero() {
-		return this.numero;
-	}
-	
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-	
 	public int getAgencia() {
 		return this.agencia;
 	}
-	
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
-	}
-	
+		
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
 	
 	public Cliente getTitular() {
 		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
 	}
 }
 
--- src/bytebank-encapsulado/src/TestaGetESet.java
+++ src/bytebank-encapsulado/src/TestaGetESet.java
@@ -1,8 +1,6 @@
 public class TestaGetESet {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.setNumero(1337);
-		System.out.println(conta.getNumero());
+		Conta conta = new Conta(1337, 24226);
 		
 		Cliente yuri = new Cliente();
 		yuri.setNome("Yuri Fernandes");
